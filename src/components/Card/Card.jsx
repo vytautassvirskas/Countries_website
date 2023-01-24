26bd0d42fde66ccdaf9f980954e9b18a02fdb70b
Vytautas Sv <vytautassvirskas@gmail.com>
2023-01-24 15:27:39 +0200

    card component structure made semantically

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -4,24 +4,24 @@ import style from "./Card.module.scss";
 const Card = ({ country, index, currentPage }) => {
   return (
     <li className={style.card}>
-      <div className={style.wrapper}>
-        <p className={style.information}>
+      <p className={style.wrapper}>
+        <span className={style.information}>
           {currentPage === 1
             ? index + 1
             : index < 9
             ? currentPage - 1 + `${index + 1}`
             : currentPage + "0"}{" "}
-        </p>
-      </div>
-      <div className={style.wrapper}>
-        <p className={style.information}>{country.name}</p>
-      </div>
-      <div className={style.wrapper}>
-        <p className={style.information}>{country.area || "no data"}</p>
-      </div>
-      <div className={style.wrapper}>
-        <p className={style.information}>{country.region}</p>
-      </div>
+        </span>
+      </p>
+      <p className={style.wrapper}>
+        <span className={style.information}>{country.name}</span>
+      </p>
+      <p className={style.wrapper}>
+        <span className={style.information}>{country.area || "no data"}</span>
+      </p>
+      <p className={style.wrapper}>
+        <span className={style.information}>{country.region}</span>
+      </p>
     </li>
   );
 };
